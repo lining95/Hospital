439a575b060818f7d2403ea6cda8af41b0066c74
usstwangshaobiao <496119805@qq.com>
2016-12-26 19:18:51 +0800

    预约系统完整

--- Reg.java
+++ Reg.java
@@ -3,26 +3,37 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Color;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
+import javax.swing.ButtonGroup;
 import javax.swing.DropMode;
 import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
 
 
 
 
 
 public class Reg {
-
+    
+     
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -53,7 +64,7 @@ public class Reg {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
-		frame.setBounds(100, 100, 572, 482);
+		frame.setBounds(100, 100, 572, 493);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -111,23 +122,122 @@ public class Reg {
 		textField_3.setBounds(119, 267, 85, 21);
 		frame.getContentPane().add(textField_3);
 		
-		JButton btnNewButton = new JButton("\u63D0\u4EA4");
-		btnNewButton.setBounds(119, 339, 93, 23);
-		frame.getContentPane().add(btnNewButton);
+		JComboBox comboBox = new JComboBox();
+		comboBox.setToolTipText("");
+		comboBox.setBounds(119, 149, 54, 21);
+	    comboBox.addItem("男");
+		comboBox.addItem("女");
+		frame.getContentPane().add(comboBox);
+			
+		JButton b1 = new JButton("\u63D0\u4EA4");
+		//单击按钮执行方法
+		b1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			
+				//输入值
+				    String S1 =textField.getText().trim();
+				    int  S3 =Integer.parseInt(textField_1.getText().trim());
+				    String S2 =(String)comboBox.getSelectedItem();
+				    String S4 =textField_2.getText().trim();
+				    String S5 =textField_3.getText().trim();
+				    String S6 =textField_4.getText().trim();
+				    
+				   //插入病人表
+					String sql1="insert into Patient values('"+S1+"','"+S2+"',"+S3+",'"+S4+"',6)";
+					Connection con = sqlOperate.Connection();
+					sqlOperate.SqlSel(sql1); 
+				//获取病人ID
+			String sql2 ="select * from Patient where PatName='"+S1+"'";
+			ResultSet rs1 = sqlOperate.SqlSel(sql2);
+			int userID=-1;
+			try {
+				while (rs1.next()){
+					userID=rs1.getInt("PatID");
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//通过科室获取医生ID
+			int doctID=-1;
+			String sql3="select DoctorID from Doctor where DoctorDeptID=(select DeptID from Department where DeptName='"+S5+"')";
+			ResultSet rs2 = sqlOperate.SqlSel(sql3);
+			try {
+				while (rs2.next()){
+					doctID=rs2.getInt("DoctorID");
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String sql4="insert into Registration values (PatientID,DoctorID,'"+S6+"','预约')";
+			sqlOperate.SqlSel(sql4);
+			JOptionPane.showMessageDialog(null,"预约成功！","提示对话框",JOptionPane.INFORMATION_MESSAGE);
+			
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
+			
+			
+			
+			
+			
+			
+		});
+		b1.setBounds(119, 360, 93, 23);
+		frame.getContentPane().add(b1);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setBackground(Color.CYAN);
-		textPane.setForeground(Color.CYAN);
-		textPane.setContentType("image/png");
-		textPane.setBounds(279, 110, 203, 175);
-		frame.getContentPane().add(textPane);
+	
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("\u7537");
-		rdbtnNewRadioButton.setBounds(121, 148, 37, 23);
-		frame.getContentPane().add(rdbtnNewRadioButton);
+		JLabel lblNewLabel_3 = new JLabel("\u9884\u7EA6\u65F6\u95F4");
+		lblNewLabel_3.setBounds(28, 310, 54, 15);
+		frame.getContentPane().add(lblNewLabel_3);
 		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("\u5973");
-		rdbtnNewRadioButton_1.setBounds(167, 148, 37, 23);
-		frame.getContentPane().add(rdbtnNewRadioButton_1);
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(119, 307, 85, 21);
+		frame.getContentPane().add(textField_4);
+		
+		JLabel label_4 = new JLabel("\uFF08\u8BF7\u8F93\u5165\u6570\u5B57\uFF09");
+		label_4.setBounds(233, 192, 109, 15);
+		frame.getContentPane().add(label_4);
+		
+		JLabel label_5 = new JLabel("(\u4E0D\u80FD\u4E3A\u7A7A)");
+		label_5.setBounds(233, 116, 69, 15);
+		frame.getContentPane().add(label_5);
+		
+		JLabel label_6 = new JLabel("\uFF08\u8BF7\u8F93\u516511\u4F4D\u624B\u673A\u53F7\uFF09");
+		label_6.setBounds(233, 230, 126, 15);
+		frame.getContentPane().add(label_6);
+		
+		JLabel label_7 = new JLabel("\uFF08\u8BF7\u8F93\u5165\u5B8C\u6574\u79D1\u5BA4\u540D\u5B57\uFF09");
+		label_7.setBounds(233, 270, 138, 15);
+		frame.getContentPane().add(label_7);
+		
+		JLabel label_8 = new JLabel("\uFF08\u4F8B\uFF1A2000/10/10 )");
+		label_8.setBounds(233, 310, 180, 15);
+		frame.getContentPane().add(label_8);
+		
+		JLabel label_9 = new JLabel("\u7D27\u6025\u60C5\u51B5\u8BF7\u62E8\u6253\uFF1A");
+		label_9.setForeground(Color.RED);
+		label_9.setBounds(28, 406, 109, 15);
+		frame.getContentPane().add(label_9);
+		
+		JLabel lbll = new JLabel("120");
+		lbll.setForeground(Color.RED);
+		lbll.setBounds(150, 406, 54, 15);
+		frame.getContentPane().add(lbll);
+		
+		JLabel lblNewLabel_4 = new JLabel("\u82E5\u6709\u7591\u95EE\uFF0C\u8BF7\u5230\u54A8\u8BE2\u5904\u54A8\u8BE2\uFF0C\u8054\u7CFB\u7535\u8BDD\uFF1A10086");
+		lblNewLabel_4.setForeground(Color.CYAN);
+		lblNewLabel_4.setBounds(28, 428, 343, 15);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		
+	   
 	}
 }
