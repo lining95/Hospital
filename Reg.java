feea1a4067634086f80587f7a26ac2ef638d2ae2
usstwangshaobiao <496119805@qq.com>
2016-12-27 02:02:27 +0800

    病人预约最终版本

--- Reg.java
+++ Reg.java
@@ -28,7 +28,7 @@ import java.awt.event.ActionEvent;
 public class Reg {
     
      
-	private JFrame frame;
+	private JFrame frame2;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -43,7 +43,7 @@ public class Reg {
 			public void run() {
 				try {
 					Reg window = new Reg();
-					window.frame.setVisible(true);
+					window.frame2.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -57,16 +57,18 @@ public class Reg {
 	public Reg() {
 		initialize();
 	}
-
+	
+		
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
-		frame.setBounds(100, 100, 572, 493);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame2 = new JFrame();
+		frame2.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frame2.setBounds(100, 100, 572, 493);
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame2.getContentPane().setLayout(null);
+		frame2.setVisible(true);
 		
 		JLabel lblNewLabel = new JLabel("xxxx\u533B\u9662");
 		lblNewLabel.setFont(new Font("宋体", Font.PLAIN, 40));
@@ -74,60 +76,60 @@ public class Reg {
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBackground(Color.ORANGE);
 		lblNewLabel.setBounds(172, 10, 187, 47);
-		frame.getContentPane().add(lblNewLabel);
+		frame2.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("\u75C5\u4EBA\u9884\u7EA6\u62A5\u8868");
 		lblNewLabel_1.setFont(new Font("宋体", Font.PLAIN, 18));
 		lblNewLabel_1.setForeground(Color.BLUE);
 		lblNewLabel_1.setBounds(28, 68, 130, 26);
-		frame.getContentPane().add(lblNewLabel_1);
+		frame2.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("\u6027\u522B");
 		lblNewLabel_2.setBounds(28, 150, 54, 19);
-		frame.getContentPane().add(lblNewLabel_2);
+		frame2.getContentPane().add(lblNewLabel_2);
 		
 		JLabel label = new JLabel("\u59D3\u540D");
 		label.setBounds(28, 110, 54, 26);
-		frame.getContentPane().add(label);
+		frame2.getContentPane().add(label);
 		
 		JLabel label_1 = new JLabel("\u5E74\u9F84");
 		label_1.setBounds(28, 190, 54, 19);
-		frame.getContentPane().add(label_1);
+		frame2.getContentPane().add(label_1);
 		
 		JLabel label_2 = new JLabel("\u8054\u7CFB\u65B9\u5F0F");
 		label_2.setBounds(28, 230, 54, 15);
-		frame.getContentPane().add(label_2);
+		frame2.getContentPane().add(label_2);
 		
 		JLabel label_3 = new JLabel("\u9884\u7EA6\u79D1\u5BA4");
 		label_3.setBounds(28, 270, 54, 15);
-		frame.getContentPane().add(label_3);
+		frame2.getContentPane().add(label_3);
 		
 		textField = new JTextField();
 		textField.setBounds(119, 113, 85, 21);
-		frame.getContentPane().add(textField);
+		frame2.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(119, 189, 85, 21);
-		frame.getContentPane().add(textField_1);
+		frame2.getContentPane().add(textField_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
 		textField_2.setBounds(119, 227, 85, 21);
-		frame.getContentPane().add(textField_2);
+		frame2.getContentPane().add(textField_2);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
 		textField_3.setBounds(119, 267, 85, 21);
-		frame.getContentPane().add(textField_3);
+		frame2.getContentPane().add(textField_3);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setToolTipText("");
 		comboBox.setBounds(119, 149, 54, 21);
 	    comboBox.addItem("男");
 		comboBox.addItem("女");
-		frame.getContentPane().add(comboBox);
+		frame2.getContentPane().add(comboBox);
 			
 		JButton b1 = new JButton("\u63D0\u4EA4");
 		//单击按钮执行方法
@@ -189,53 +191,53 @@ public class Reg {
 			
 		});
 		b1.setBounds(119, 360, 93, 23);
-		frame.getContentPane().add(b1);
+		frame2.getContentPane().add(b1);
 		
 	
 		
 		JLabel lblNewLabel_3 = new JLabel("\u9884\u7EA6\u65F6\u95F4");
 		lblNewLabel_3.setBounds(28, 310, 54, 15);
-		frame.getContentPane().add(lblNewLabel_3);
+		frame2.getContentPane().add(lblNewLabel_3);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
 		textField_4.setBounds(119, 307, 85, 21);
-		frame.getContentPane().add(textField_4);
+		frame2.getContentPane().add(textField_4);
 		
 		JLabel label_4 = new JLabel("\uFF08\u8BF7\u8F93\u5165\u6570\u5B57\uFF09");
 		label_4.setBounds(233, 192, 109, 15);
-		frame.getContentPane().add(label_4);
+		frame2.getContentPane().add(label_4);
 		
 		JLabel label_5 = new JLabel("(\u4E0D\u80FD\u4E3A\u7A7A)");
 		label_5.setBounds(233, 116, 69, 15);
-		frame.getContentPane().add(label_5);
+		frame2.getContentPane().add(label_5);
 		
 		JLabel label_6 = new JLabel("\uFF08\u8BF7\u8F93\u516511\u4F4D\u624B\u673A\u53F7\uFF09");
 		label_6.setBounds(233, 230, 138, 15);
-		frame.getContentPane().add(label_6);
+		frame2.getContentPane().add(label_6);
 		
 		JLabel label_7 = new JLabel("\uFF08\u8BF7\u8F93\u5165\u5B8C\u6574\u79D1\u5BA4\u540D\u5B57\uFF09");
 		label_7.setBounds(233, 270, 163, 15);
-		frame.getContentPane().add(label_7);
+		frame2.getContentPane().add(label_7);
 		
 		JLabel label_8 = new JLabel("\uFF08\u4F8B\uFF1A2000/10/10 )");
 		label_8.setBounds(233, 310, 180, 15);
-		frame.getContentPane().add(label_8);
+		frame2.getContentPane().add(label_8);
 		
 		JLabel label_9 = new JLabel("\u7D27\u6025\u60C5\u51B5\u8BF7\u62E8\u6253\uFF1A");
 		label_9.setForeground(Color.RED);
 		label_9.setBounds(28, 406, 109, 15);
-		frame.getContentPane().add(label_9);
+		frame2.getContentPane().add(label_9);
 		
 		JLabel lbll = new JLabel("120");
 		lbll.setForeground(Color.RED);
 		lbll.setBounds(150, 406, 54, 15);
-		frame.getContentPane().add(lbll);
+		frame2.getContentPane().add(lbll);
 		
 		JLabel lblNewLabel_4 = new JLabel("\u82E5\u6709\u7591\u95EE\uFF0C\u8BF7\u5230\u54A8\u8BE2\u5904\u54A8\u8BE2\uFF0C\u8054\u7CFB\u7535\u8BDD\uFF1A10086");
 		lblNewLabel_4.setForeground(Color.CYAN);
 		lblNewLabel_4.setBounds(28, 428, 343, 15);
-		frame.getContentPane().add(lblNewLabel_4);
+		frame2.getContentPane().add(lblNewLabel_4);
 		
 		
 	   
